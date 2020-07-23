89cea1410250f6a73a7f08a0e7e2737cc4f2c5a9
hrofarrow <hrofarrow@gmail.com>
2020-07-23 13:57:49 -0400

    Completed analysis for both parts of project

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "769fa8a3381aec7610408e74a47fbe0b"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCeqfoKZPN1dK4nzhLciOr4rWjaagOWE24"
